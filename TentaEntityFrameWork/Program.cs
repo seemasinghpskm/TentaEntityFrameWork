400c14e5216e869425101d1609d3ebc203944faf
seema <seema@MININT-L0C6J1I>
2016-11-25 11:33:01 +0100

    resubmit

--- TentaEntityFrameWork/Program.cs
+++ TentaEntityFrameWork/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TentaEntityFrameWork.Models;
 
 namespace TentaEntityFrameWork
 {
@@ -10,6 +11,37 @@ namespace TentaEntityFrameWork
     {
         static void Main(string[] args)
         {
+            //OrdersPerEmployee(); //fråga 4
+           // CustomersWithNamesLongerThan25Characters(); //Fråga5//
+        }
+
+        private static void CustomersWithNamesLongerThan25Characters()
+        {
+            using (NorthWindEntityContext cx = new NorthWindEntityContext())
+            {
+
+                var names = from n in cx.Customers
+                            where n.CompanyName.Length > 25
+                            select n.CompanyName;
+                foreach (var name in names) 
+                {
+                    Console.WriteLine(name);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void OrdersPerEmployee()
+        {
+            using (NorthWindEntityContext cx = new NorthWindEntityContext())
+            {
+                foreach (var data in cx.Employees)
+                {
+
+                }
+           
+            }
         }
     }
 }
